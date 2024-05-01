3228b280f2ba10d97c682433961fad00ac5d8fb6
Igor <111295359+Tirensky@users.noreply.github.com>
2024-05-01 21:02:37 +0300

    Add drag-on script

--- static/script.js
+++ static/script.js
@@ -97,6 +97,29 @@ $(document).ready(function(){
             $("#addImageModal").css("display", "none");
         }
     });
+
+    const dropContainer = document.getElementById("dropcontainer")
+    const fileInput = document.getElementById("images")
+    
+    dropContainer.addEventListener("dragover", (e) => {
+        // prevent default to allow drop
+        e.preventDefault()
+    }, false)
+    
+    dropContainer.addEventListener("dragenter", () => {
+        dropContainer.classList.add("drag-active")
+    })
+    
+    dropContainer.addEventListener("dragleave", () => {
+        dropContainer.classList.remove("drag-active")
+    })
+    
+    dropContainer.addEventListener("drop", (e) => {
+        e.preventDefault()
+        dropContainer.classList.remove("drag-active")
+        fileInput.files = e.dataTransfer.files
+    })
+
 });
 
 document.addEventListener("DOMContentLoaded", function() {
