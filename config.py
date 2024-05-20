de6fd1ec99d8881b890df8f167bc4423ffd66817
Igor <111295359+Tirensky@users.noreply.github.com>
2024-05-21 01:45:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 app.secret_key = 'gotohellbobbykotick'  # Секретный ключ для сеансов. Нужен для обеспечения безопасности сессий на стороне клиента.
 
 # Конфигурация Flask-Mail
-app.config['MAIL_SERVER'] = 'smtp.yandex.ru'  # Адрес SMTP сервера
+app.config['MAIL_SERVER'] = 'smtp.yandex.ru'  # Адрес SMTP сервера (yandex.ru, google.com, mail.ru)
 app.config['MAIL_PORT'] = 587  # Порт SMTP сервера (обычно 587 для TLS или 465 для SSL)
 app.config['MAIL_USE_TLS'] = True  # Использовать ли TLS
-app.config['MAIL_USERNAME'] = 'i.pavluschin@ya.ru'  # Ваше имя пользователя на почтовом сервере
+app.config['MAIL_USERNAME'] = 'sample@mail.com'  # Ваше почта на почтовом сервере
 app.config['MAIL_PASSWORD'] = 'iyrirtmegxqynjam'  # Ваш пароль на почтовом сервере
 app.config['MAIL_DEFAULT_SENDER'] = ('GuardiArt', 'i.pavluschin@ya.ru')
 
